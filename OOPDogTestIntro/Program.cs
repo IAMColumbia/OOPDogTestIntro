0e17320b6e9dc3dc61370590e09d39925b875f74
Jeff Meyers <jmeyers@colum.edu>
2020-04-23 11:31:07 -0500

    Add project files.

---
+++ OOPDogTestIntro/Program.cs
@@ -0,0 +1,22 @@
+﻿using DogLibrary;
+using System;
+
+namespace OOPDogTestIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dog Tests....");
+            Dog d = new Dog();
+            //Test Happy Birthday
+            Console.WriteLine(d.About());
+            d.HappyBirthday();
+            Console.WriteLine(d.About());
+
+            //Test Eat
+            d.Eat();
+            Console.WriteLine(d.About());
+        }
+    }
+}
